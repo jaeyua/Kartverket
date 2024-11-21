e6f58650ef8d1f56375ca16bc0f91fb88a0269bf
Aaron Silber <aarons@uia.no>
2024-11-21 14:24:17 +0100

    endret dokumentasjon AppDbContext.cs

--- Nettside/Data/AppDbContext.cs
+++ Nettside/Data/AppDbContext.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Nettside.Models;
+using System.Collections.Generic;
 
 namespace Nettside.Data
 {
+    // DbContext-klasse for applikasjonens database som arver fra IdentityDbContext for bruk av brukerautentisering.
     public class AppDbContext : IdentityDbContext<Users>
     {
+        // Konstruktør som setter opp databasens konfigurasjon ved å bruke de gitte alternativene.
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
-        // Legg til en DbSet for GeoChanges
+        // DbSet for GeoChanges-tabellen som inneholder geodata for endringer.
         public DbSet<GeoChanges> GeoChange { get; set; }
-        public DbSet<AreaChange> AreaChanges { get; set; }   
 
-
+        // DbSet for AreaChanges-tabellen som holder oversikt over de ulike områdeendringene.
+        public DbSet<AreaChange> AreaChanges { get; set; }
     }
 }
