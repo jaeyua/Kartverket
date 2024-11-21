4091fab713a9777adfbe1032b9d0b15f249a30e5
Aaron Silber <aarons@uia.no>
2024-11-21 14:35:19 +0100

    Endret dokumentasjon ProgilePageViewModel

--- Nettside/ViewModels/ProfilePageViewModel.cs
+++ Nettside/ViewModels/ProfilePageViewModel.cs
@@ -1,12 +1,15 @@
 ﻿namespace Kartverket16.Models.ViewModels
 {
+    // ViewModel som representerer informasjonen som vises på brukerens profilside.
     public class ProfilePageViewModel
     {
-
+        // Brukerens fornavn.
         public string FirstName { get; set; }
+
+        // Brukerens etternavn.
         public string LastName { get; set; }
+
+        // Brukerens e-postadresse.
         public string Email { get; set; }
-
-
     }
 }
