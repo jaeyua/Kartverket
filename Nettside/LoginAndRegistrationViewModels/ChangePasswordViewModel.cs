82ab10a99eb56c70e7b7c6862623e3a436594663
Aaron Silber <aarons@uia.no>
2024-11-21 14:27:15 +0100

    Endret dokumentasjon ChangePasswordViewModel

--- Nettside/LoginAndRegistrationViewModels/ChangePasswordViewModel.cs
+++ Nettside/LoginAndRegistrationViewModels/ChangePasswordViewModel.cs
@@ -2,12 +2,15 @@
 
 namespace UsersApp.ViewModels
 {
+    // ViewModel for endring av passord, som inneholder felt for e-post og passord.
     public class ChangePasswordViewModel
     {
+        // E-postfelt som er påkrevd og må være en gyldig e-postadresse.
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
 
+        // Nytt passord som er påkrevd, med minimum 8 tegn og maksimum 40 tegn, og det må bekrefte passordet.
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
         [DataType(DataType.Password)]
@@ -15,6 +18,7 @@ namespace UsersApp.ViewModels
         [Compare("ConfirmNewPassword", ErrorMessage = "Password does not match.")]
         public string NewPassword { get; set; }
 
+        // Bekreftelse på det nye passordet som er påkrevd.
         [Required(ErrorMessage = "Confirm Password is required.")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm New Password")]
