64a6ec98214303f567d54295a3195f7d6442e3b4
Aaron Silber <aarons@uia.no>
2024-11-21 14:28:07 +0100

    Endret LoginViewModel.cs

--- Nettside/LoginAndRegistrationViewModels/LoginViewModel.cs
+++ Nettside/LoginAndRegistrationViewModels/LoginViewModel.cs
@@ -2,16 +2,20 @@
 
 namespace UsersApp.ViewModels
 {
+    // ViewModel for innlogging, som inneholder felt for e-post, passord og husk meg-valg.
     public class LoginViewModel
     {
+        // E-postfelt som er påkrevd og må være en gyldig e-postadresse.
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
 
+        // Passordfelt som er påkrevd og skal behandles som et passord.
         [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        // Boolsk verdi for å angi om brukeren skal huskes ved neste pålogging.
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
