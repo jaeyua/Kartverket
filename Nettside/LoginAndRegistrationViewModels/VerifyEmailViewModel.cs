a9bee1b4dc3b7a5c263fad0d5f89759b994c1ecc
Aaron Silber <aarons@uia.no>
2024-11-21 14:29:02 +0100

    Endret dokumentasjon VerifyEmailViewModel

--- Nettside/LoginAndRegistrationViewModels/VerifyEmailViewModel.cs
+++ Nettside/LoginAndRegistrationViewModels/VerifyEmailViewModel.cs
@@ -2,8 +2,10 @@
 
 namespace UsersApp.ViewModels
 {
+    // ViewModel for e-postverifisering, som inneholder et felt for e-postadressen.
     public class VerifyEmailViewModel
     {
+        // E-postfelt som er påkrevd og må være en gyldig e-postadresse.
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
