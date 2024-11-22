d0f7d1abe8b386d07fa6323fb7f0f370bf25774c
Tringak1 <tringakongjeli@hotmail.com>
2024-11-22 14:20:24 +0100

    Configure Identity and Role-Based Authentication with EF Core and seed migrations in Program.cs

--- Nettside/Program.cs
+++ Nettside/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Nettside.Data;
 using Nettside.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,20 +13,66 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseMySql(builder.Configuration.GetConnectionString("MariaDbConnection"),
 new MySqlServerVersion(new Version(10, 5, 9))));
 
+
+// configuring identity
 builder.Services.AddIdentity<Users, IdentityRole>(options =>
 {
+    // password settings
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequiredLength = 8;
     options.Password.RequireUppercase = false;
+
+    // user settings
     options.User.RequireUniqueEmail = true;
     options.SignIn.RequireConfirmedAccount = false;
     options.SignIn.RequireConfirmedEmail = false;
     options.SignIn.RequireConfirmedPhoneNumber = false;
+
+
+    // lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(25);
+    options.Lockout.MaxFailedAccessAttempts = 10;
+    options.Lockout.AllowedForNewUsers = true;
+
+
 })
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
+
+
+// configure authentication and authorization
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login/";
+    options.AccessDeniedPath = "/Account/AccessDenied/";
+});
+
+
+
+
 var app = builder.Build();
+
+// run migrations and add seed-data
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        // run migrations
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "en feil oppstod under migrasjon eller seeding");
+    }
+
+
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
